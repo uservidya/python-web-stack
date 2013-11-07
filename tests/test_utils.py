e65cc1fb1623f9b9e002a7a37ceda58d17a6f4f9
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-07 17:48:57 +0800

    More util tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os.path
 from nose.tools import ok_, raises
 from pywebstack.formulae import Formula
 from pywebstack import utils
@@ -9,6 +10,18 @@ from pywebstack import utils
 ALL_FORMULAE_NAMES = (
     'django', 'Django'
 )
+
+
+def test_normalize():
+    ok_(utils.normalize('..', 'test_utils.py'), __file__)
+
+
+def test_chdir():
+    current = os.getcwd()
+    outer = os.path.abspath(utils.normalize(current, '..'))
+    with utils.chdir('..'):
+        ok_(os.getcwd(), outer)
+    ok_(os.getcwd(), current)
 
 
 def test_get_formula_class():
@@ -25,3 +38,8 @@ def test_get_formula():
     for name in ALL_FORMULAE_NAMES:
         formula = utils.get_formula(name, 'test_project')
         ok_(isinstance(formula, Formula))
+
+
+if __name__ == '__main__':
+    import nose
+    nose.run(argv=[__file__, '--with-doctest', '-vv'])
